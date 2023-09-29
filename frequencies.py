49c0080d5fca4ea1801bb647d8c9f4392dd322fe
RickSanchez88E <110410215+RickSanchez88E@users.noreply.github.com>
2023-09-29 08:22:22 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,6 @@
-"""Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        key = str(item) if not isinstance(item, str) else item
+        frequencies[key] = frequencies.get(key, 0)+1
     return frequencies
